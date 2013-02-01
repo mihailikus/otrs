805b56f9caec1c3b9671746503e6198872300048
Michael Volkov <miha@mihol.ru>
2013-02-01 16:26:12 +0400

    Command-line option --no_gui anabled

--- main.cpp
+++ main.cpp
@@ -4,9 +4,31 @@
 
 int main(int argc, char *argv[])
 {
+    // Под Windows устанавливаем кодировку cp1251
+    #ifdef Q_WS_WIN
+    QTextCodec *codec = QTextCodec::codecForName("Windows-1251");
+    // Под остальными ОС - utf8
+    #else
+    QTextCodec *codec = QTextCodec::codecForName("UTF-8");
+    #endif
+
+    bool viewGUI = true;
+    QString param;
+    for (int i = 0; i<argc; i++) {
+        param = codec->toUnicode(argv[i]);
+
+        if (param == "--no_gui") {
+            viewGUI = false;
+        }
+    }
+
+
+
     QApplication a(argc, argv);
     otrs w;
-    w.show();
+    if (viewGUI) {
+        w.show();
+    }
 
 
     return a.exec();
